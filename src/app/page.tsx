a22af6f28b8fdf6aa07ff26f76c3bf377de682ae
Kusalana Buddhadasa <kslnrnmyr@gmail.com>
2024-11-09 18:49:22 +0530

    shadcnui crash course completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,65 @@
-export default function Home() {
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+
+//delay response
+await new Promise ((resolve)=> setTimeout(resolve,3000))
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
   return (
     <main>
-      home
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex flex-row gap-4 items-center">
+
+              <Avatar>
+
+                <AvatarImage src={`/img/${recipe.image}`} alt={recipe.title}/>
+                <AvatarFallback>
+                  {recipe.title.slice(0,2)}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge  variant='secondary'>Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
-  )
+  );
 }
